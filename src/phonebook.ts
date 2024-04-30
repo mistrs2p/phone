621d8e2d3a45d9ddd55f3a3d3562ca994bc6f843
mistrain <mahdimousavi40@gmail.com>
2024-04-30 10:34:15 +0330

    add functionality

--- src/phonebook.ts
+++ src/phonebook.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import readline from "readline";
+import { phoneBook } from "./main";
 
 interface PhoneBookEntry {
   name: string;
@@ -12,8 +13,9 @@ export const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-let phoneEntry: Partial<PhoneBookEntry> = {
+let phoneEntry: PhoneBookEntry = {
   name: "",
+  phoneNumber: "",
 };
 
 async function getNumber(): Promise<string> {
@@ -34,13 +36,41 @@ async function getName(): Promise<string> {
   });
 }
 
-export async function createPhoneBookEntry(): Promise<void> {
+async function isExistingEntry(phoneNumber: string): Promise<Boolean> {
+  return await new Promise((resolve, reject) => {
+    const existingEntry = phoneBook.find(
+      (entry) => entry.phoneNumber === phoneNumber
+    );
+    if (!existingEntry) {
+      reject(false);
+    } else {
+      resolve(true);
+    }
+  });
+}
+
+export async function createPhoneBookEntry(): Promise<string> {
   phoneEntry = {
     phoneNumber: await getNumber(),
     name: await getName(),
   };
   console.log(phoneEntry);
+  return await new Promise(async (resolve, reject) => {
+    const isExistNumber = await isExistingEntry(phoneEntry.phoneNumber);
+    // rl.close();
 
+    if (isExistNumber) {
+      reject(
+        new Error(
+          `Error: the number is already exists in our Database :(( \nName: ${phoneEntry.name}`
+        )
+      );
+    } else {
+      phoneBook.push(phoneEntry);
+      savePhoneBook(phoneBook);
+      resolve("Your number successfully saved :))");
+    }
+  });
   // return new Promise((resolve, reject) => {
   //   rl.question("Enter ur number: ", (phoneNumber: string) => {
   //     rl.question("Enter ur name: ", (name: string) => {
