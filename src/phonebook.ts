e686ce8acee6e0d5b5c0513ab5992c5ddfe34258
mistrain <mahdimousavi40@gmail.com>
2024-05-01 16:01:00 +0330

    add existing name and refactor code

--- src/phonebook.ts
+++ src/phonebook.ts
@@ -1,9 +1,10 @@
 import { PhoneBookEntry } from "./interfaces";
 import { loadPhonBook, savePhoneBook } from "./storage";
 import { getName, getNumber } from "./userentries";
+import { generateUniqueId } from "./uniqeid";
 const phoneBook = loadPhonBook();
 
-function isExistingEntry(phoneEntry: PhoneBookEntry): Promise<Boolean> {
+function isExistingEntryPhone(phoneEntry: PhoneBookEntry): Promise<Boolean> {
   return new Promise((resolve, reject) => {
     const existingEntry = phoneBook.find(
       (entry) => entry.phoneNumber === phoneEntry.phoneNumber
@@ -20,15 +21,31 @@ function isExistingEntry(phoneEntry: PhoneBookEntry): Promise<Boolean> {
   });
 }
 
+function isExistingName(phoneEntry: PhoneBookEntry): Promise<Boolean> {
+  return new Promise((resolve, reject) => {
+    const existingName = phoneBook.find(
+      (entry) => entry.name === phoneEntry.name
+    );
+    if (existingName) {
+      resolve(true);
+    } else {
+      resolve(false);
+    }
+  });
+}
+
 export async function createPhoneBookEntry() {
   try {
     const phoneEntry: PhoneBookEntry = {
+      id: generateUniqueId(),
       name: await getName(),
       phoneNumber: await getNumber(),
     };
 
-    const isExistNumber = await isExistingEntry(phoneEntry);
-
+    const isExistName = await isExistingName(phoneEntry);
+    const isExistNumber = await isExistingEntryPhone(phoneEntry);
+    if (isExistName)
+      console.log(`Found same name in data base with name: ${phoneEntry.name}`);
     if (isExistNumber === false) {
       phoneBook.push(phoneEntry);
       savePhoneBook(phoneBook);
