34a58a318c31860d882e74fd830f2816aa27df03
mistrain <mahdimousavi40@gmail.com>
2024-05-01 16:00:43 +0330

    cleansing

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
 import { createPhoneBookEntry } from "./phonebook";
 import { rl } from "./userentries";
-
 async function main() {
   await createPhoneBookEntry()
     .then((response) => {
-      console.log("response", response);
+      console.log("response => ", response);
     })
     .catch((error) => {
       console.log("Main error => ", error);
     })
     .finally(() => {
-      rl.question("R u want to continiue: ", (answer) => {
+      rl.question("Do you want to continiue: ", (answer) => {
         if (/^(y|yes)$/i.test(answer.toLowerCase())) {
           main();
         } else {
-          rl.close()
-          process.exit(0)
+          rl.close();
+          process.exit(0);
         }
       });
     });
-
 }
 
 main();
