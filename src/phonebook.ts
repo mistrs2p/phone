8d82f179a09cc81ab84ab1f23ecc1a6c17ac3ca4
MistRain <mahdimousavi40@gmail.com>
2024-05-18 10:47:37 +0330

    refactor code by new storage method

--- src/phonebook.ts
+++ src/phonebook.ts
@@ -1,50 +1,37 @@
 import { PhoneBookEntry } from "./interfaces";
-import { save, phoneBook } from "./storage";
+import { save, findEntryByPhoneNumber, findEntryByName } from "./storage";
 import { getName, getPhoneNumber } from "./userentries";
-import { generateUniqueId } from "./uniqeid";
+// import { generateUniqueId } from "./uniqeid";
 
-function isExistingEntryPhone(phoneEntry: PhoneBookEntry): Promise<Boolean> {
-  return new Promise((resolve, reject) => {
-    const existingEntry = phoneBook.find(
-      (entry) => entry.phoneNumber === phoneEntry.phoneNumber
+async function isExistingEntryPhone(phoneEntry: PhoneBookEntry): Promise<Boolean> {
+  const existingEntry = await findEntryByPhoneNumber(phoneEntry.phoneNumber);
+  if (existingEntry) {
+    throw new Error(
+      `The number already exists in our database :(( \nName: ${existingEntry.name}`
     );
-    if (existingEntry) {
-      reject(
-        new Error(
-          `the number is already exists in our Database :(( \nName: ${phoneEntry.name}`
-        )
-      );
-    } else {
-      resolve(false);
-    }
-  });
+  }
+  return false;
 }
 
-function isExistingName(phoneEntry: PhoneBookEntry): Promise<Boolean> {
-  return new Promise((resolve, reject) => {
-    const existingName = phoneBook.find(
-      (entry) => entry.name === phoneEntry.name
-    );
-    if (existingName) {
-      resolve(true);
-    } else {
-      resolve(false);
-    }
-  });
+async function isExistingName(phoneEntry: PhoneBookEntry): Promise<Boolean> {
+  const existingName = await findEntryByName(phoneEntry.name);
+  return !!existingName;
 }
 
 export async function createPhoneBookEntry() {
   try {
+
+    const name = await getName()
+    const phoneNumber = await getPhoneNumber()
     const phoneEntry: PhoneBookEntry = {
-      id: generateUniqueId(),
-      name: await getName(),
-      phoneNumber: await getPhoneNumber(),
+      name,
+      phoneNumber
     };
 
     await isExistingName(phoneEntry);
     await isExistingEntryPhone(phoneEntry);
     
-    save(phoneEntry);
+    await save(phoneEntry);
     return "Your number successfully saved :))";
     
   } catch (err) {
