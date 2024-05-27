30edf7cf4472e37bcce8a4517b7f138b6f7dac5d
MistRain <mahdimousavi40@gmail.com>
2024-05-27 11:00:26 +0330

    refactor and easify code

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,23 +1,17 @@
 // storage.ts
-import { PhoneBookEntry } from "../interfaces/inedx";
 import { initDb } from "../database/db";
-import { IStorageEngine, IPhoneBookLookup } from "../interfaces/inedx";
+import { PhoneBookEntry, IStorageEngine } from "../interfaces";
 import { SQLiteStorageEngine } from "../database/sqliteEngine";
 import { JSONStorageEngine } from "../database/jsonEngine";
-import { SQLiteLookup } from "../lookup/sqliteLookup";
-import { JSONLookup } from "../lookup/jsonLookup";
 require("dotenv").config();
 
 let storageEngine: IStorageEngine;
-let phoneBookLookup: IPhoneBookLookup;
 
 export async function initStorageEngine() {
   if (process.env.STORAGE_ENGINE === "json") {
     storageEngine = new JSONStorageEngine();
-    phoneBookLookup = new JSONLookup();
   } else {
     storageEngine = new SQLiteStorageEngine();
-    phoneBookLookup = new SQLiteLookup();
     await initDb();
   }
 }
@@ -28,16 +22,4 @@ export async function load(): Promise<PhoneBookEntry[]> {
 
 export async function save(phoneEntry: PhoneBookEntry): Promise<void> {
   return storageEngine.save(phoneEntry);
-}
-
-export async function findEntryByPhoneNumber(
-  phoneNumber: string
-): Promise<PhoneBookEntry | null> {
-  return phoneBookLookup.findEntryByPhoneNumber(phoneNumber);
-}
-
-export async function findEntryByName(
-  name: string
-): Promise<PhoneBookEntry | null> {
-  return phoneBookLookup.findEntryByName(name);
 }
