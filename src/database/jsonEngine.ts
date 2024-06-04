52886145fe118354c996b6c798927e6a4651f732
MistRain <mahdimousavi40@gmail.com>
2024-06-04 12:57:18 +0330

    add constructor and get var path from env

--- src/database/jsonEngine.ts
+++ src/database/jsonEngine.ts
@@ -1,13 +1,26 @@
 import { IStorageEngine, PhoneBookEntry, FindType } from "../interfaces";
-import { phoneBookFilePath } from "../config/filepathes";
 import fs from "fs";
+require("dotenv").config();
+
+
 export class JSONStorageEngine implements IStorageEngine {
+
+  constructor(private path:string = process.env.JSON_PATH!) {
+    if (!this.path) {
+      throw new Error("JSON_PATH is not defined in environment variables.");
+    }
+  }
+
+  async init(): Promise<void> {
+    console.log("JSON storage engine SETUP");
+    return  
+  }
   async load(): Promise<PhoneBookEntry[]> {
     try {
-      // if (!fs.existsSync(phoneBookFilePath)) {
-      //   fs.writeFileSync(phoneBookFilePath, JSON.stringify([]));
-      // }
-      const data = fs.readFileSync(phoneBookFilePath, "utf8");
+      if (!fs.existsSync(this.path)) {
+        fs.writeFileSync(this.path, JSON.stringify([]));
+      }
+      const data = fs.readFileSync(this.path, "utf8");
       const entries: PhoneBookEntry[] = data ? JSON.parse(data) : [];
       return entries;
     } catch (error) {
@@ -20,7 +33,7 @@ export class JSONStorageEngine implements IStorageEngine {
     try {
       const entries = await this.load();
       entries.push(phoneEntry);
-      fs.writeFileSync(phoneBookFilePath, JSON.stringify(entries, null, 2));
+      fs.writeFileSync(this.path, JSON.stringify(entries, null, 2));
       console.log("Entry saved.");
     } catch (error) {
       console.error("Error saving entry:", error);
