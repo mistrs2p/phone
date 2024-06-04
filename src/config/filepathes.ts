8819200ce0495b5f4b58c6d73f72e03dff43c7a3
MistRain <mahdimousavi40@gmail.com>
2024-06-04 12:58:57 +0330

    remove files

--- src/config/filepathes.ts
+++
@@ -1,1 +0,0 @@
-export const phoneBookFilePath = "phonebook.json";
--- src/utils/uniqeid.ts
+++
@@ -1,4 +0,0 @@
-export function generateUniqueId(): string {
-  // Generate a random alphanumeric ID
-  return Math.random().toString(36).substr(2, 9);
-}
