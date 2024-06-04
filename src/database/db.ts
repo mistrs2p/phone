b18f9826a17a2ba2e57c847498a55bd6bf958deb
MistRain <mahdimousavi40@gmail.com>
2024-06-04 12:56:43 +0330

    refactor

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,28 +1,23 @@
 import sqlite3 from "sqlite3";
-
+import { readFileSync } from "fs";
+import { join } from "path";
 export async function openDb() {
   return new sqlite3.Database("phonebook.db");
 }
 
 export async function initDb() {
+  const sqlFilePath = join(__dirname, "database.sql");
+  console.log(sqlFilePath)
+  const sqlContent = readFileSync(sqlFilePath, "utf8");
   const db = await openDb();
   db.serialize(() => {
-    db.run(
-      `
-      CREATE TABLE IF NOT EXISTS phonebook (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        phoneNumber TEXT NOT NULL UNIQUE
-      )
-    `,
-      (err) => {
-        if (err) {
-          console.error("Error creating table:", err.message);
-        } else {
-          console.log("\nPhonebook table created or already exists.");
-        }
+    db.run(sqlContent, (err) => {
+      if (err) {
+        console.error("Error creating table:", err.message);
+      } else {
+        console.log("\nPhonebook table created or already exists.");
       }
-    );
+    });
   });
 
   return db;
