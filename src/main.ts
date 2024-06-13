0bc08ae40ed858da9fbffec7d5dba0070bf4b2bd
MistRain <mahdimousavi40@gmail.com>
2024-06-13 10:17:47 +0330

    config app with comander pkg

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createPhoneBookEntry } from "./services/phonebook";
 import { rl } from "./utils/readline";
 import { initStorageEngine } from "./storage";
+import { Command } from "commander";
 
 require("dotenv").config();
 
+const program = new Command();
+
+program
+  .version("1.0.0")
+  .option("-e, --engine <type>", "set storage engine (json or sqlite)", "json")
+  .option("-p, --path <path>", "set path for JSON storage", "phonebook.json");
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log(options);
+process.env.STORAGE_ENGINE = options.engine;
+if (options.engine === "json") {
+  process.env.JSON_PATH = options.path;
+}
 async function main() {
   await initStorageEngine();
   createPhoneBookEntry()
