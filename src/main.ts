67f3d5d2c5feaca753315dbe4971feeac3443bde
MistRain <mahdimousavi40@gmail.com>
2024-06-22 16:28:55 +0330

    add action logic and check existance of env variables

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,31 @@ const program = new Command();
 program
   .version("1.0.0")
   .option("-e, --engine <type>", "set storage engine (json or sqlite)", "json")
-  .option("-p, --path <path>", "set path for JSON storage", "phonebook.json");
+  .option("-p, --path <path>", "set path for JSON storage", "phonebook.json")
+  .action((options) => {
+    console.log(process.env);
+
+    if (!("JSON_PATH" in process.env)) {
+      // throw new Error("An error occurred while geting JSON PATH Address");
+      console.log("An error occurred while geting JSON PATH Address");
+      process.exit(1);
+    }
+    if (!("STORAGE_ENGINE" in process.env)) {
+      // throw new Error("An error occurred while geting JSON Engin Address");
+      console.log("An error occurred while geting JSON Engin Address");
+      process.exit(1);
+    }
+
+    process.env.JSON_PATH = options.path;
+    process.env.STORAGE_ENGINE = options.engine;
+
+    main();
+  });
 
 program.parse(process.argv);
 
-const options = program.opts();
-console.log(options);
-process.env.STORAGE_ENGINE = options.engine;
-if (options.engine === "json") {
-  process.env.JSON_PATH = options.path;
-}
+// const options = program.opts();
+// console.log(options);
 async function main() {
   await initStorageEngine();
   createPhoneBookEntry()
@@ -40,5 +55,3 @@ async function main() {
       });
     });
 }
-
-main();
