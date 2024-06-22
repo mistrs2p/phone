a11de876471264e2ab4ebb801e3858ea5e29c8c5
MistRain <mahdimousavi40@gmail.com>
2024-06-22 18:22:06 +0330

    refactor code to prevent using .env and use ram

--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,41 @@ import { createPhoneBookEntry } from "./services/phonebook";
 import { rl } from "./utils/readline";
 import { initStorageEngine } from "./storage";
 import { Command } from "commander";
+import { EnginePath } from "./interfaces";
 
-require("dotenv").config();
-
+// require("dotenv").config();
+export const engineAndPath: EnginePath = {
+  path: "",
+  engine: "",
+};
 const program = new Command();
 
 program
   .version("1.0.0")
-  .option("-e, --engine <type>", "set storage engine (json or sqlite)", "json")
-  .option("-p, --path <path>", "set path for JSON storage", "phonebook.json")
-  .action((options) => {
-    // console.log(process.env);
-
-    if (!("JSON_PATH" in process.env)) {
-      // throw new Error("An error occurred while geting JSON PATH Address; Make sure you have environment file or directory or spell checking");
-      console.log("An error occurred while geting JSON PATH Address; Make sure you have environment file or directory or spell checking");
-      process.exit(1);
+  .option("-eng, --engine <engine>", "set storage engine (json or sqlite)")
+  .option("-p, --path <path>", "set path for JSON storage", "phonebook")
+  .action((options, r) => {
+    console.log(options);
+    console.log(r);
+    // console.log(options)
+    switch (options.engine) {
+      case "json":
+        engineAndPath.path = options.path + ".json";
+        break;
+      case "sqlite":
+        engineAndPath.path = options.path + ".db";
+        break;
+      default:
+        throw new Error("Unknown engine: " + options.engine + ". Please specify the right engine");
     }
-    if (!("STORAGE_ENGINE" in process.env)) {
-      // throw new Error("An error occurred while geting JSON Engin Address; Make sure you have environment file or directory or spell checking");
-      console.log("An error occurred while geting JSON Engin Address; Make sure you have environment file or directory or spell checking");
-      process.exit(1);
-    }
-
-    process.env.JSON_PATH = options.path;
-    process.env.STORAGE_ENGINE = options.engine;
+    engineAndPath.engine = options.engine;
+    console.log(engineAndPath);
 
     main();
   });
 
 program.parse(process.argv);
-console.log("process.argv", process.argv);
+// console.log("process.argv", process.argv);
 // const options = program.opts();
 // console.log(options);
 async function main() {
