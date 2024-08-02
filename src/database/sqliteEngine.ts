733f6172907956b154b937ab79a693ac92003cb4
MistRain <mahdimousavi40@gmail.com>
2024-08-02 15:21:31 +0330

    add fn for init and open sqlite db

--- src/database/sqliteEngine.ts
+++ src/database/sqliteEngine.ts
@@ -1,5 +1,32 @@
 import { IStorageEngine, PhoneBookEntry, FindType } from "../interfaces";
-import { openDb, initDb } from "./db";
+import sqlite3 from "sqlite3";
+import { readFileSync } from "fs";
+import { join } from "path";
+import {engineAndPath} from "../main" 
+
+export async function openDb() {
+  return new sqlite3.Database(engineAndPath.path);
+}
+
+export async function initDb() {
+  const sqlFilePath = join(__dirname, "database.sql");
+  console.log(sqlFilePath)
+  const sqlContent = readFileSync(sqlFilePath, "utf8");
+  const db = await openDb();
+  db.serialize(() => {
+    db.run(sqlContent, (err) => {
+      if (err) {
+        console.error("Error creating table:", err.message);
+      } else {
+        console.log("\nPhonebook table created or already exists.");
+      }
+    });
+  });
+
+  return db;
+}
+
+
 export class SQLiteStorageEngine implements IStorageEngine {
   async init(): Promise<void> {
     console.log("SQLITE storage engine SETUP");
